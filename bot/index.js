952483751661b4fb031bb664436755c6c931b6d1
badlyWay <vlad_zyshchuk@mail.ru>
2024-03-18 16:53:06 +0300

    edit: change start bot event

--- bot/index.js
+++ bot/index.js
@@ -6,16 +6,22 @@ const bot = new TelegramBot(process.env.API_KEY_BOT, {
     autoStart: true,
 });
 
-bot.on("text", async msg => {
+bot.onText(/\/start/, async msg => {
     try {
-        const chatId = msg.chat.id;
-        await bot.sendMessage(chatId, "Обработка... ");
-        await kafka.sendMessage({
-            chatId,
-            url: msg.text,
+        await bot.sendMessage(msg.chat.id, `Привет ${msg.from.username || msg.from.first_name}!\nОтправь мне ссылку статьи и я попробую её озвучить.`);
+
+        bot.on("message", async msg => {
+            const chatId = msg.chat.id;
+            await bot.sendMessage(chatId, "Обработка.\nЭто может занять некоторое время. ");
+            await kafka.sendMessage({
+                chatId,
+                url: msg.text,
+            });
         });
+
     } catch (error) {
         console.error(error);
+        bot.sendMessage("Что-то пошло не так...\n", error);
     }
 });
 
